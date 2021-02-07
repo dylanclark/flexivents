4af9625db3ed832361e7b58494d6bbc19a793a92
Dylan Clark <dylanmcdanielclark@gmail.com>
2021-02-07 14:35:58 -0800

    add the code

---
+++ src/flexivents.h
@@ -0,0 +1,136 @@
+﻿#pragma once
+
+#include <functional>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <exception>
+#include <string>
+#include <any>
+
+// move event in 
+// deletions
+// ID check
+namespace Events {
+
+	class EventNotRegisteredException : public std::exception
+	{
+	public:
+		EventNotRegisteredException(const std::string& id)
+		{
+			static const std::string prefix{ "The event with id " };
+			static const std::string suffix{ " was not registered with the EventsRegistrar." };
+			m_what = { prefix + id + suffix };
+		}
+
+		~EventNotRegisteredException() noexcept = default;
+
+		virtual const char* what() const noexcept override
+		{
+			return m_what.c_str();
+		}
+
+	private:
+		std::string m_what;
+	};
+
+	template<typename ...TEventArgs>
+	class Event
+	{
+	public:
+		using TCallbackFunction = std::function<void(TEventArgs...)>;
+
+		void RegisterCallback(TCallbackFunction callback)
+		{
+			m_callbacks.push_back(callback);
+		}
+
+		template<typename ...TEventArgs>
+		void Raise(TEventArgs... args)
+		{
+			for (const auto& callback : m_callbacks)
+			{
+				callback(args...);
+			}
+		}
+
+	private:
+		std::vector<TCallbackFunction> m_callbacks;
+	};
+
+	template<typename ...TEventArgs>
+	class EventManager
+	{
+	public:
+
+		using TCallbackFunction = typename Event<TEventArgs...>::TCallbackFunction;
+
+		void RegisterEvent(const std::string& id, Event<TEventArgs...> evt)
+		{
+			m_events.insert({ id, evt });
+		}
+
+		void RegisterCallback(const std::string& id, TCallbackFunction callback)
+		{
+			FindEvent(id).RegisterCallback(callback);
+		}
+
+		template<typename ...TEventArgs>
+		void RaiseEvent(const std::string& id, TEventArgs... args)
+		{
+			FindEvent(id).Raise(args...);
+		}
+
+	private:
+		std::unordered_map<std::string, Event<TEventArgs...>> m_events;
+
+		Event<TEventArgs...>& FindEvent(const std::string& id) {
+			const auto& eventIterator{ m_events.find(id) };
+			if (eventIterator != m_events.end()) {
+				return eventIterator->second;
+			}
+			throw EventNotRegisteredException(id);
+		}
+	};
+
+	class EventsAggregator
+	{
+	public:
+
+		template<typename ...TEventArgs>
+		void RegisterEvent(const std::string& id)
+		{
+			Event<TEventArgs...> event{};
+			GetEventManager<TEventArgs...>(id).RegisterEvent(id, event);
+		}
+
+		template<typename ...TEventArgs>
+		void RegisterCallback(const std::string& id, std::function<void(TEventArgs...)> callback)
+		{
+			GetEventManager<TEventArgs...>(id).RegisterCallback(id, callback);
+		}
+
+		template<typename ...TEventArgs>
+		void RaiseEvent(const std::string& id, TEventArgs... args)
+		{
+			GetEventManager<TEventArgs...>(id).RaiseEvent(id, args...);
+		}
+
+	private:
+		template<typename ...TEventArgs>
+		EventManager<TEventArgs...>& GetEventManager(const std::string& id)
+		{
+			const auto eventManagerIterator{ eventManagers.find(id) };
+			if (eventManagerIterator == eventManagers.end())
+			{
+				EventManager<TEventArgs...> manager{};
+				eventManagers.insert({ id, manager });
+			}
+			return std::any_cast<EventManager<TEventArgs...>&>(eventManagers[id]);
+		}
+
+		std::unordered_map<std::string, std::any> eventManagers;
+	};
+
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+﻿// Events.cpp : Defines the entry point for the application.
+//
+
+#include "flexivents.h"
+
+#include <iostream>
+
+using namespace Events;
+
+void pl(const std::string& s) {
+	std::cout << s << std::endl;
+}
+
+
+#define DefineEvent(EVENT_NAME, CALLBACK_INTERFACE_NAME, ...) \
+	const std::string EVENT_NAME { #EVENT_NAME };    \
+    using CALLBACK_INTERFACE_NAME = Event<__VA_ARGS__>::TCallbackFunction;
+
+namespace MyEvents
+{
+
+	DefineEvent(VoidEvent, IVoidEventCallback, void);
+	DefineEvent(BoolEvent, IBooleanEventCallback, bool);
+	DefineEvent(MultiEvent, IMultiEventCallback, int, int);
+}
+
+Event<void>::TCallbackFunction voidCallback{
+	[]() {
+		pl("void event");
+	}
+};
+
+std::function<void(bool)> boolCallback{
+	[](bool b)
+	{
+		pl("bool event");
+		std::cout << b << std::endl;
+	}
+};
+
+std::function<void(int, int)> multiCallback{
+	[](int a, int b) {
+		pl("multi event");
+		std::cout << a << " times " << b << " equals " << a * b << std::endl;
+	}
+};
+
+int main()
+{
+	std::unique_ptr<EventsAggregator> ea{ std::make_unique<EventsAggregator>() };
+
+	ea->RegisterEvent(MyEvents::VoidEvent);
+	ea->RegisterEvent<bool>(MyEvents::BoolEvent);
+	ea->RegisterEvent<int, int>(MyEvents::MultiEvent);
+
+    ea->RegisterCallback(MyEvents::VoidEvent, voidCallback);
+	ea->RegisterCallback<bool>(MyEvents::BoolEvent, boolCallback);
+	ea->RegisterCallback<int, int>(MyEvents::MultiEvent, multiCallback);
+
+	ea->RaiseEvent(MyEvents::BoolEvent, false);
+	ea->RaiseEvent(MyEvents::BoolEvent, true);
+	ea->RaiseEvent(MyEvents::MultiEvent, 6, 9);
+	ea->RaiseEvent(MyEvents::VoidEvent);
+
+	try {
+		ea->RegisterCallback<bool>("TEST", boolCallback);
+	}
+	catch (const EventNotRegisteredException& e)
+	{
+		std::cout << e.what();
+	}
+	return 0;
+}
